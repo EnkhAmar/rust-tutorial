2a9c47b9859891eea184f50b76b813eaac8be70f
EnkhAmar <enkhamar1220@gmail.com>
2025-10-22 11:12:51 +0800

    variables

--- variables/target/debug/deps/variables.d
+++
@@ -1,5 +0,0 @@
-C:\Users\enkha\dev\rust_projects\variables\target\debug\deps\variables.d: src\main.rs
-
-C:\Users\enkha\dev\rust_projects\variables\target\debug\deps\variables.exe: src\main.rs
-
-src\main.rs:
--- variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-C:\Users\enkha\dev\rust_projects\variables\target\debug\variables.exe: C:\Users\enkha\dev\rust_projects\variables\src\main.rs
