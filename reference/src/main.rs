a24442855f2771789802019c0019ee4cfa005e94
EnkhAmar <enkhamar1220@gmail.com>
2025-10-27 15:42:48 +0800

    reference

---
+++ reference/src/main.rs
@@ -0,0 +1,16 @@
+// fn main() {
+//     let m1 = String::from("Hello");
+//     let m2 = String::from("world");
+//     greet(&m1, &m2); // note the ampersands
+//     let s = format!("{} {}", m1, m2);
+// }
+
+// fn greet(g1: &String, g2: &String) { // note the ampersands
+//     println!("{} {}!", g1, g2);
+// }
+
+fn main() {
+    let x = Box::new(0);
+    let y = Box::new(&x);
+    println!("{}", ***y);
+}
